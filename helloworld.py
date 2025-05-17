679da9459b511bd076dbd85ac57cc771b3baf47b
shishir-2-05-01 <shishiryolov8@gmail.com>
2025-05-17 22:55:30 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -2,3 +2,5 @@
 print("Hello world")
 
 print("Update: This update will reflect once Jenkins auto builds after 5 minutes.")
+
+print("Update 2: This update will reflect once Jenkins auto builds after 5 minutes.")
